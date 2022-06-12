2f2d32e057bba73c8ed726d6278db67cdb1002fd
Yash <bajajy82@gmail.com>
2022-06-12 12:55:35 +0530

    Backdrop added to Modal

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -5,16 +5,18 @@ import rules from "../images/image-rules.svg";
 
 const Modal = ({ onToggle }) => {
   return ReactDOM.createPortal(
-    <div className="modal-container">
-      <div className="modal-box">
-        <div className="modal__header">
-          <h1>Rules</h1>
-          <button onClick={onToggle}>
-            <img src={close} alt="close" />
-          </button>
-        </div>
-        <div>
-          <img src={rules} alt="rules" />
+    <div className="modal">
+      <div className="modal-container">
+        <div className="modal-box">
+          <div className="modal__header">
+            <h1>Rules</h1>
+            <button onClick={onToggle}>
+              <img src={close} alt="close" />
+            </button>
+          </div>
+          <div>
+            <img src={rules} alt="rules" />
+          </div>
         </div>
       </div>
     </div>,
